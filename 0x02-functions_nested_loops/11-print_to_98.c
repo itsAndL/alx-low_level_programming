fa4706dd5db72832453c6cb66f1729ccb5c7a64b
ayoub <ayoub003lkebch@gmail.com>
2023-03-21 14:03:32 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,12 @@
 #include "main.h"
 #include "6-abs.c"
 #include <stdio.h>
-
+/**
+ * print_to_98 - print from n to 98
+ * @n: the nbr of type integer
+ *
+ * Return: print 'til 98
+ */
 void print_to_98(int n)
 {
 	int i;
