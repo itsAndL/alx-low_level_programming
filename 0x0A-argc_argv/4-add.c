4b23607002d4e34e8ffcf90be6285b4352d8f016
itsAndL <ayoub003lkebch@gmail.com>
2023-04-10 15:13:07 +0000

    Task: 4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,58 +1,57 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-/**
- * StringCheck - checks string
- * @s: string to check
- * Return: boolean
- */
-int StringCheck(char *s)
-{
-	int i = 0;
+#include <stdio.h> /* printf */
+#include <stdlib.h> /* atoi */
+#include <stdbool.h> /* bool data type */
 
-	for (; s[i] != '\0'; i++)
+/**
+ * is_num - iterate through each argv to test if it's a number
+ * @argvv: a argv
+ * Return: true only if entire string is a number, false if not
+ */
+bool is_num(char *argvv)
+{
+	int j = 0;
+
+	for (j = 0; argvv[j]; j++)
 	{
-		if (!isdigit(s[i]))
-		{
+		if (!(argvv[j] >= '0' && argvv[j] <= '9'))
 			return (0);
-		}
 	}
 	return (1);
 }
+
 /**
  * main - entry point
  * @argc: number of command-line arguments
  * @argv: array of actual arguments
  *
- * Return: 1 if the arguments passed aren't correct and 0 otherwise
+ * Return: Always 0.
  *
  */
-int main(int argc, char  *argv[])
+int main(int argc, char *argv[])
 {
-	int i;
-	int result = 0;
+	int i = 1;
+	int sum = 0;
 
-	if (argc > 1)
+	/* validate input */
+	if (argc == 1)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			if (StringCheck(argv[i]))
-			{
-				result += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		printf("%d\n", result);
+		printf("0\n");
 		return (0);
 	}
-	else
-	{
-		printf("%d\n", 0);
-		return (1);
-	}
 
+	/* check all arguments to add numbers */
+	while (i < argc)
+	{
+		if (is_num(argv[i]))
+			sum += atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		i++;
+	}
+	printf("%d\n", sum);
+
+	return (0);
 }
